07f6bd9c092bdd3e58b5e042745947cccdb1bc19
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-08-22 08:53:57 -0500

    Create Auto.java
    Creaciòn clase Auto

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,38 @@
+package test;
+
+public class Auto {
+  String modelo;
+  int precio;
+  Asiento [] asientos;
+  String marca;
+  Motor motor;
+  int registro;
+  static int cantidadCreados;
+
+    int cantidadAsientos() {
+      int numAsientos=0;
+      for (int z=0; z<asientos.length; z++) {
+        if(asientos[z]!=null) { 
+          numAsientos++;
+        }
+      }
+      return numAsientos;
+    }
+    String verificarIntegridad() {
+      boolean verificar=true;
+      if (motor.registro!=registro){
+        verificar=false
+      }
+      for(int z=0; z<asientos.length; z++){
+        if(asientos[z]!=null && asientos[z].registro!=registro){
+          verificar=false
+            break;
+        }
+      }
+      if(verificar=true){
+        return "Auto original";
+      } else {
+        return "Las piezas no son originales";
+      }
+  }
+}
